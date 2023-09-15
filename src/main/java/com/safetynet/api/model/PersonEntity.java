02653d8b23d2450fc3a083419a9754ab129e7138
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-15 13:59:04 +0200

    create model PersonEntity

--- src/main/java/com/safetynet/api/model/PersonEntity.java
+++ src/main/java/com/safetynet/api/model/PersonEntity.java
@@ -1,5 +1,36 @@
 package com.safetynet.api.model;
 
-public class PersonEntity {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
+@RequiredArgsConstructor
+//create getter with name of attributs
+@Accessors(fluent = true) @Getter
+@Setter
+public class PersonEntity {
+	
+		  private String id;
+		  private String firstName;
+		  private String lastName;
+		  private String address;
+		  private int zip;
+		  private String city;
+		  private String phone;
+		  private String email;
+
+		  @Override
+		  public String toString() {
+		      return "Person{" +
+		              "id=" + id +
+		              ", first name='" + firstName + '\'' +
+		              ", last name=" + lastName +
+		              ", address=" + address +
+		              ", zip=" + zip +
+		              ", city=" + city +
+		              ", phone=" + phone +
+		              ", email=" + email +
+		              '}';
+		  }
 }
