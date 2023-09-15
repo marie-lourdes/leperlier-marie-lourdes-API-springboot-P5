e636378d94f4bb8c4e7b208e41e533d667785f90
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-15 17:53:02 +0200

    create model MedicalReport

--- src/main/java/com/safetynet/api/model/MedicalReportEntity.java
+++ src/main/java/com/safetynet/api/model/MedicalReportEntity.java
@@ -1,5 +1,52 @@
 package com.safetynet.api.model;
 
-public class MedicalReportEntity {
+import java.util.List;
 
+public class MedicalReportEntity {
+	
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String birthdate;
+	private List<String> medications;
+	private List<String> allergies;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public List<String> getMedications() {
+		return medications;
+	}
+	public void setMedications(List<String> medications) {
+		this.medications = medications;
+	}
+	public List<String> getAllergies() {
+		return allergies;
+	}
+	public void setAllergies(List<String> allergies) {
+		this.allergies = allergies;
+	}
+	
+	
 }
