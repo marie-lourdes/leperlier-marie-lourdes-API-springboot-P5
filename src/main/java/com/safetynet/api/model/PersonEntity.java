1889c2fc732ee283767a951ed5a7a6ee381a070f
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-16 18:14:00 +0200

    test request person entity and login tomcat

--- src/main/java/com/safetynet/api/model/PersonEntity.java
+++ src/main/java/com/safetynet/api/model/PersonEntity.java
@@ -1,7 +1,14 @@
 package com.safetynet.api.model;
 
-public class PersonEntity {
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity 
+public class PersonEntity {
+	 @Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
 	private String id;
 	private String firstName;
 	private String lastName;
--- src/main/java/com/safetynet/api/repository/IPersonRepository.java
+++ src/main/java/com/safetynet/api/repository/IPersonRepository.java
@@ -1,5 +1,16 @@
 package com.safetynet.api.repository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IPersonRepository {
+import com.safetynet.api.model.PersonEntity;
+public interface IPersonRepository extends CrudRepository<PersonEntity, Integer>{
+
+
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+
 
 }
+
