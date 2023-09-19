ae1f3cbe68473673d6d873bb78b1a4c91f7fb01b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-19 21:17:25 +0200

    PersonService.java:create methods getAllPerson () and saveOnePerson()

--- src/main/java/com/safetynet/api/service/dataservice/PersonService.java
+++ src/main/java/com/safetynet/api/service/dataservice/PersonService.java
@@ -1,5 +1,26 @@
 package com.safetynet.api.service.dataservice;
 
-public class PersonService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.safetynet.api.model.Person;
+import com.safetynet.api.repository.IPersonRepository;
+
+@Service
+public class PersonService {
+	@Autowired
+    private IPersonRepository personRepository;
+
+ 
+    public Iterable<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    
+    public Person saveOnePerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return savedPerson;
+    }
+    
+ 
 }
