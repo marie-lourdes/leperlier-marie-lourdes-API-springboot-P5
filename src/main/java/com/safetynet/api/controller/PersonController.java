6903296390ad0228aaaedf01045e7d204e3eab81
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-21 12:49:49 +0200

    PersonController.java: add comments

--- src/main/java/com/safetynet/api/controller/PersonController.java
+++ src/main/java/com/safetynet/api/controller/PersonController.java
@@ -2,7 +2,6 @@ package com.safetynet.api.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.safetynet.api.model.Person;
 import com.safetynet.api.service.dataservice.PersonService;
 
-@RestController 
+@RestController
 //@RequestMapping(path = "/api) // This means URL's start with /demo (after Application path)
 public class PersonController {
 
 	@Autowired
 	private PersonService personService;
 
-	@PostMapping(value = "/person") 
+	@PostMapping(value = "/person")
 	public Person createPerson(@RequestParam String firstName, @RequestParam String lastName,
 			@RequestParam String address, @RequestParam String city, @RequestParam int zip, @RequestParam String phone,
 			@RequestParam String email) {
@@ -54,38 +53,40 @@ public class PersonController {
 		// This returns a JSON or XML with the users
 		return personService.getAllPersons();
 	}
-	
+
 	@GetMapping("/person/{id}")
-	public Optional<Person>getOnePerson(@PathVariable Long id) {
+	public Optional<Person> getOnePerson(@PathVariable Long id) {
 		return personService.getOnePersonById(id);
 	}
-	
+
+	// l id , le nom et prénom ne sont pas modifiable
 	@PutMapping("/person/{id}")
-	public Optional<Person> updateOnePersonById(@PathVariable Long id,@RequestBody Person personModified) {
-		 Optional<Person>	personFoundById = Optional.ofNullable(personService.getOnePersonById(id).orElseThrow(() -> new NullPointerException(" an error has occured,this person"+id+"don't exist, try again ")));
-		 if(id ==personFoundById.get().getId() ) {
-		 personFoundById.get().setAddress(personModified.getAddress()); 
-		 personFoundById.get().setZip(personModified.getZip()); 
-		 personFoundById.get().setCity(personModified.getCity());
-		 personFoundById.get().setPhone(personModified.getPhone()); 
-		 personService.savePerson( personFoundById.get()); 
-		 }
-		 return personFoundById; 
+	public Optional<Person> updateOnePersonById(@PathVariable Long id, @RequestBody Person personModified) {
+		Optional<Person> personFoundById = Optional.ofNullable(personService.getOnePersonById(id).orElseThrow(
+				() -> new NullPointerException(" an error has occured,this person" + id + "don't exist, try again ")));
+		if (id == personFoundById.get().getId()) {
+			personFoundById.get().setAddress(personModified.getAddress());
+			personFoundById.get().setZip(personModified.getZip());
+			personFoundById.get().setCity(personModified.getCity());
+			personFoundById.get().setPhone(personModified.getPhone());
+			personService.savePerson(personFoundById.get());
+		}
+		return personFoundById;
 	}
-	
+
 	@DeleteMapping("/person")
-	public 	ResponseEntity<Long> deleteOnePersonByName (@RequestParam String firstName, @RequestParam String lastName) {
-		List <Person> persons =(List<Person>) personService.getAllPersons();
-		persons.forEach(elem->{ 			
-			    String firstNamePerson =elem.getFirstName(); 
-			    String lastNamePerson =elem.getLastName()	;
-		    if(firstNamePerson.contains(firstName) &&  lastNamePerson.contains(lastName)  ) {
-		    	personService.deleteOnePersonByName(elem);     
-		    }	
-			});
-		
-		return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		
+	public ResponseEntity<Long> deleteOnePersonByName(@RequestParam String firstName, @RequestParam String lastName) {
+		List<Person> persons = (List<Person>) personService.getAllPersons();
+		persons.forEach(elem -> {
+			String firstNamePerson = elem.getFirstName();
+			String lastNamePerson = elem.getLastName();
+			if (firstNamePerson.contains(firstName) && lastNamePerson.contains(lastName)) {
+				personService.deleteOnePersonByName(elem);
+			}
+		});
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
 	}
 
 }
