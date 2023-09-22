a9a81142d6d78570761788fe32afcb391237b91c
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-22 18:30:35 +0200

    remove comment

--- src/main/java/com/safetynet/api/repository/IFireStationRepository.java
+++ src/main/java/com/safetynet/api/repository/IFireStationRepository.java
@@ -8,16 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.safetynet.api.model.FireStation;
 
 public interface IFireStationRepository  extends CrudRepository<FireStation, Long>{
-    /*     @Override
-      default   public void delete( FireStation fireStation) {
    
-       List<String> listOfFireStation = new ArrayList<String>();
-         	listOfFireStation.add(fireStation.getStationNumber().toString());
-         	listOfFireStation.add(fireStation.getAddress());
-        	listOfFireStation.remove(0);
-        	
-         }*/
-	   
-		
-	
+	 	
 }
