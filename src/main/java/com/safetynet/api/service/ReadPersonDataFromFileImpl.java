3da01e2d45ff344c33405b96489c09520f38cf3e
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-29 13:39:49 +0200

    ReadPersonDataFromFileImpl.java: test with mappin getObjectStream the entity Person of file Json

--- src/main/java/com/safetynet/api/service/ReadPersonDataFromFileImpl.java
+++ src/main/java/com/safetynet/api/service/ReadPersonDataFromFileImpl.java
@@ -1,34 +1,50 @@
 package com.safetynet.api.service;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-
-
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
+import javax.json.stream.JsonParserFactory;
 
 public class ReadPersonDataFromFileImpl implements IDatasFileReader {
-	 private String path = "src/main/resources/datasSafetyNetAlerts.json"; 
- @SuppressWarnings("rawtypes")
-@Override
-public void readFile() throws FileNotFoundException {
-	Object ob = new JSONParser(new FileReader( path ));
-
-	JSONObject js =  (JSONObject) ob;
-	
-	 JSONArray personData = null;
-	try {
-		   personData = js.getJSONArray("phoneNumbers");
+	private String path = "src/main/resources/datasSafetyNetAlerts.json";
+	 private JsonArray phones;
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void readFile() throws FileNotFoundException {
+		InputStream is = new FileInputStream(path);
+		  
+		JsonParserFactory factory = Json.createParserFactory(null);
+		JsonParser parser = factory.createParser(is, StandardCharsets.UTF_8);
 		
-	} catch (JSONException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		if (!parser.hasNext() && parser.next() != JsonParser.Event.START_OBJECT) {
+			return;
+		}
 
-     System.out.println("list Person is: " + personData);
- }
+		while (parser.hasNext()) {
+
+			Event event = parser.next();
+
+			// starting Array Person
+			if (event == JsonParser.Event.START_ARRAY) {
+				 while (parser.hasNext()) {
+
+	                    event = parser.next();
+	                    phones =( JsonArray ) parser.getObjectStream().filter(e->e.getKey().equals("phone"))
+	                    	                             .map(e->e.getValue())
+	                    	                             .findFirst()
+	                    	                             .get();
+				 }
+
+			}
+		}
+		
+		//System.out.println(phones);
+	}
 
 }
