383e8c6cd1f9398e3772d3854f1abef80967bfe8
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-29 14:33:17 +0200

    ReadPersonDataFromFileImpl.java: test JsonParser of javax.json.Json

--- src/main/java/com/safetynet/api/service/ReadPersonDataFromFileImpl.java
+++ src/main/java/com/safetynet/api/service/ReadPersonDataFromFileImpl.java
@@ -4,24 +4,27 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonValue;
 import javax.json.stream.JsonParser;
 import javax.json.stream.JsonParser.Event;
 import javax.json.stream.JsonParserFactory;
 
 public class ReadPersonDataFromFileImpl implements IDatasFileReader {
 	private String path = "src/main/resources/datasSafetyNetAlerts.json";
-	 private JsonArray phones;
+	private JsonArray phones;
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void readFile() throws FileNotFoundException {
 		InputStream is = new FileInputStream(path);
-		  
+
 		JsonParserFactory factory = Json.createParserFactory(null);
 		JsonParser parser = factory.createParser(is, StandardCharsets.UTF_8);
-		
+
 		if (!parser.hasNext() && parser.next() != JsonParser.Event.START_OBJECT) {
 			return;
 		}
@@ -32,19 +35,22 @@ public class ReadPersonDataFromFileImpl implements IDatasFileReader {
 
 			// starting Array Person
 			if (event == JsonParser.Event.START_ARRAY) {
-				 while (parser.hasNext()) {
+				int val = 0;
+				while (parser.hasNext()) {
+					event = parser.next();
+					if (event == JsonParser.Event.KEY_NAME) {
+						String key = toString();
+						event = parser.next();
+						if (key.equals("persons")) {
+						
+							phones = parser.getArray();
+						}
 
-	                    event = parser.next();
-	                    phones =( JsonArray ) parser.getObjectStream().filter(e->e.getKey().equals("phone"))
-	                    	                             .map(e->e.getValue())
-	                    	                             .findFirst()
-	                    	                             .get();
-				 }
+						System.out.println(phones + ":" + ++val);
+					}
+				}
 
 			}
 		}
-		
-		//System.out.println(phones);
 	}
-
 }
