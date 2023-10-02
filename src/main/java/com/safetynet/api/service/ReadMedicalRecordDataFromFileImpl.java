eb85ae3ef64537e71c15ee5a60cdc29604d97c2b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-02 08:17:04 +0200

    ReadMedicalRecordDataFromFileImpl.java: stringfy each value of JsonArray of medications and JsonArray of allergie of medicalrecords

--- src/main/java/com/safetynet/api/service/ReadMedicalRecordDataFromFileImpl.java
+++ src/main/java/com/safetynet/api/service/ReadMedicalRecordDataFromFileImpl.java
@@ -3,6 +3,7 @@ package com.safetynet.api.service;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,7 +12,6 @@ import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonValue;
-import javax.json.stream.JsonParser;
 
 import org.springframework.stereotype.Component;
 
@@ -27,37 +27,51 @@ public class ReadMedicalRecordDataFromFileImpl implements IDatasFileReader {
 	public List<MedicalRecord> readFile() throws IOException {
 		InputStream is = new FileInputStream(path);
 		JsonReader jsonReader = Json.createReader(is);
-		 listOfMedicalRecords= new LinkedList<MedicalRecord>();
-		
-		// get JsonObject from JsonReader
+		listOfMedicalRecords = new LinkedList<MedicalRecord>();
+
+		// get JsonArray from JsonReader
 		JsonObject datasJsonMedicalRecordsObject = jsonReader.readObject();
-		datasJsonMedicalRecords= datasJsonMedicalRecordsObject.getJsonArray("medicalrecords");
+		datasJsonMedicalRecords = datasJsonMedicalRecordsObject.getJsonArray("medicalrecords");
 		System.out.println("all datas Json MedicalRecord parsed with its arrays:" + datasJsonMedicalRecords);
-		
+
 		jsonReader.close();
 		is.close();
-		
-		//create list linked of medicalRecords
-		for( JsonValue elem : datasJsonMedicalRecords) {
-			MedicalRecord medicalRecord =new MedicalRecord(); 
-			medicalRecord.setFirstName( elem.asJsonObject().getString("firstName"));
-			medicalRecord.setLastName( elem.asJsonObject().getString("lastName"));
-			medicalRecord.setBirthdate( elem.asJsonObject().getString("birthdate"));
-			List<String> medicationArray =	(List<String>) elem.asJsonObject().get("medications");
-			medicalRecord.setMedications( medicationArray);
-			List<String> allergiesArray =	(List<String>) elem.asJsonObject().get("allergies");
-			medicalRecord.setAllergies( allergiesArray);
-			
-			
-			 listOfMedicalRecords.add(medicalRecord);
-			System.out.println("element of medicalRecords"+elem.asJsonObject());
-			
-		}
-		// listOfMedicalRecords.add();
-		System.out.println("list of medicalRecords"+ listOfMedicalRecords);
 
-		return  listOfMedicalRecords ;
+		// create list linked of medicalRecords
+		for (JsonValue elem : datasJsonMedicalRecords) {
+			MedicalRecord medicalRecord = new MedicalRecord();
+			medicalRecord.setFirstName(elem.asJsonObject().getString("firstName"));
+			medicalRecord.setLastName(elem.asJsonObject().getString("lastName"));
+			medicalRecord.setBirthdate(elem.asJsonObject().getString("birthdate"));
+			
+			// create ArrayList of medications from JsonArray medications
+			JsonArray medicationsArray = elem.asJsonObject().getJsonArray("medications");
+			List<String> listOfMedications = new ArrayList<String>();
+
+			int indexMedications = 0;
+			for (JsonValue value : medicationsArray) {
+				listOfMedications.add(indexMedications++, (String) value.toString());
+			}
+			medicalRecord.setMedications(listOfMedications);
+			
+			// create ArrayList of allergies from JsonArray allergies
+			JsonArray allergiesArray = elem.asJsonObject().getJsonArray("allergies");
+			List<String> listOfAllergies = new ArrayList<String>();
+
+			int indexAllergies = 0;
+			for (JsonValue value : allergiesArray) {
+				listOfAllergies.add(indexAllergies++, (String) value.toString());
+			}
+			medicalRecord.setAllergies(listOfAllergies);
+
+			listOfMedicalRecords.add(medicalRecord);
+			System.out.println("element of medicalRecords" + elem.asJsonObject());
+
+		}
+		System.out.println("list of medicalRecords" + listOfMedicalRecords);
+
+		return listOfMedicalRecords;
 		// return datasJsonPersonParsed ;
-	
+
 	}
 }
