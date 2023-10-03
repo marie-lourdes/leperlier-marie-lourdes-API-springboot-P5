1e34aeaa897ef637f2a96e98d0f1203ceaddbc8b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-03 07:41:09 +0200

    create class UploadFireStatiionFileService and deplace method getFireStationsFromFile of UploadDataService

--- src/main/java/com/safetynet/api/service/UploadFireStationDataFileService.java
+++ src/main/java/com/safetynet/api/service/UploadFireStationDataFileService.java
@@ -1,5 +1,18 @@
 package com.safetynet.api.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.safetynet.api.model.FireStation;
+
 public class UploadFireStationDataFileService {
 
+	@Autowired
+	ReadFireStationDataFromFileImpl  readFireStations;
+	
+	public List<FireStation>getFireStationsFromFile() throws IOException {
+		 return	readFireStations.readFile();
+		}
 }
