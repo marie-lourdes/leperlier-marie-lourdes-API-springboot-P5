60b3c0c0932782db393bf2eaf7c6d44d75699fce
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-03 10:07:29 +0200

    UploadMedicalRecordDataFileService.java: implements Interface MedicalRecordREADONLYRepository and ovveride its method findAll() with call method readFile() ofReadMedicalRecordDataFromFileImpl

--- src/main/java/com/safetynet/api/service/UploadMedicalRecordDataFileService.java
+++ src/main/java/com/safetynet/api/service/UploadMedicalRecordDataFileService.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.safetynet.api.model.MedicalRecord;
+import com.safetynet.api.repository.IMedicalRecordREADONLYRepository;
 @Service
-public class UploadMedicalRecordDataFileService {
+public class UploadMedicalRecordDataFileService implements IMedicalRecordREADONLYRepository  {
 	@Autowired
 	ReadMedicalRecordDataFromFileImpl readMedicalRecords;
 
 	public List<MedicalRecord> getMedicalRecordsFromFile() throws IOException {
+		return readMedicalRecords.readFile();
+	}
+	
+	@Override
+	public List<MedicalRecord> findAll() throws IOException{
 		return readMedicalRecords.readFile();
 	}
 }
