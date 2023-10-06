a8f4c7736b06e84461d580eb22570ba79e98a3d7
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-06 22:49:40 +0200

    remove unecessary import in classes implemntation of IDataFileReader

--- src/main/java/com/safetynet/api/service/ReadFireStationDataFromFileImpl.java
+++ src/main/java/com/safetynet/api/service/ReadFireStationDataFromFileImpl.java
@@ -1,15 +1,11 @@
 package com.safetynet.api.service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.json.Json;
 import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.json.JsonValue;
 
 import org.springframework.stereotype.Component;
@@ -17,37 +13,35 @@ import org.springframework.stereotype.Component;
 import com.safetynet.api.model.FireStation;
 
 @Component
-public class ReadFireStationDataFromFileImpl implements IDatasFileReader <FireStation>{
-	private String path = "src/main/resources/datasSafetyNetAlerts.json";
+public class ReadFireStationDataFromFileImpl implements IDatasFileReader<FireStation> {
 	private JsonArray datasJsonFireStations;
 	private List<FireStation> listOfFireStations;
 
 	@Override
 	public List<FireStation> readFile() throws IOException {
 		listOfFireStations = new LinkedList<FireStation>();
-		
-		// get JsonArray of data entity  from JsonReader of Interface  IDatasFileReader 
-		 //creer une methode dans l Interface IDatasFileReader  pour generer  JsonArray et 
-		//eviter de repeter de tous les class impl  le path
-		 datasJsonFireStations= readDataJson("firestations" ); 
-		//create list linked of fireStations
-		
-		for( JsonValue elem : datasJsonFireStations) {
-			FireStation fireStation =new FireStation(); 
-			fireStation.setId(elem.asJsonObject().getString("station")  );
-			fireStation.setStationNumber( elem.asJsonObject().getString("station"));
-			fireStation.setAddress( elem.asJsonObject().getString("address"));
-			
-			
-			 listOfFireStations.add(fireStation);
-			System.out.println("element of fireStations"+elem.asJsonObject());
-			
+
+		// get JsonArray of data entity from JsonReader of Interface IDatasFileReader
+		// creer une methode dans l Interface IDatasFileReader pour generer JsonArray et
+		// eviter de repeter de tous les class impl le path
+		datasJsonFireStations = readDataJson("firestations");
+
+		// create list linked of fireStations
+		for (JsonValue elem : datasJsonFireStations) {
+			FireStation fireStation = new FireStation();
+			fireStation.setId(elem.asJsonObject().getString("station"));
+			fireStation.setStationNumber(elem.asJsonObject().getString("station"));
+			fireStation.setAddress(elem.asJsonObject().getString("address"));
+
+			listOfFireStations.add(fireStation);
+			System.out.println("element of fireStations" + elem.asJsonObject());
+
 		}
 		// listOfFireStations.add();
-		System.out.println("list of fireStations"+ listOfFireStations);
+		System.out.println("list of fireStations" + listOfFireStations);
 
-		return  listOfFireStations ;
+		return listOfFireStations;
 		// return datasJsonPersonParsed ;
 	}
-	
+
 }
--- src/main/java/com/safetynet/api/service/ReadMedicalRecordDataFromFileImpl.java
+++ src/main/java/com/safetynet/api/service/ReadMedicalRecordDataFromFileImpl.java
@@ -1,16 +1,12 @@
 package com.safetynet.api.service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.json.Json;
 import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.json.JsonValue;
 
 import org.springframework.stereotype.Component;
@@ -19,7 +15,6 @@ import com.safetynet.api.model.MedicalRecord;
 
 @Component
 public class ReadMedicalRecordDataFromFileImpl implements IDatasFileReader<MedicalRecord> {
-	private String path = "src/main/resources/datasSafetyNetAlerts.json";
 	private JsonArray datasJsonMedicalRecords;
 	private List<MedicalRecord> listOfMedicalRecords;
 
@@ -27,17 +22,18 @@ public class ReadMedicalRecordDataFromFileImpl implements IDatasFileReader<Medic
 	public List<MedicalRecord> readFile() throws IOException {
 		listOfMedicalRecords = new LinkedList<MedicalRecord>();
 
-		// get JsonArray of data entity  from JsonReader of Interface  IDatasFileReader 
-		datasJsonMedicalRecords= readDataJson("medicalrecords" ); 
-		
+		// get JsonArray of data entity from JsonReader of Interface IDatasFileReader
+		datasJsonMedicalRecords = readDataJson("medicalrecords");
+
 		// create list linked of medicalRecords
 		for (JsonValue elem : datasJsonMedicalRecords) {
 			MedicalRecord medicalRecord = new MedicalRecord();
-			medicalRecord.setId( elem.asJsonObject().getString("firstName") + " " + elem.asJsonObject().getString("lastName"));
+			medicalRecord.setId(
+					elem.asJsonObject().getString("firstName") + " " + elem.asJsonObject().getString("lastName"));
 			medicalRecord.setFirstName(elem.asJsonObject().getString("firstName"));
 			medicalRecord.setLastName(elem.asJsonObject().getString("lastName"));
 			medicalRecord.setBirthdate(elem.asJsonObject().getString("birthdate"));
-			
+
 			// create ArrayList of medications from JsonArray medications
 			JsonArray medicationsArray = elem.asJsonObject().getJsonArray("medications");
 			List<String> listOfMedications = new ArrayList<String>();
@@ -47,7 +43,7 @@ public class ReadMedicalRecordDataFromFileImpl implements IDatasFileReader<Medic
 				listOfMedications.add(indexMedications++, (String) value.toString());
 			}
 			medicalRecord.setMedications(listOfMedications);
-			
+
 			// create ArrayList of allergies from JsonArray allergies
 			JsonArray allergiesArray = elem.asJsonObject().getJsonArray("allergies");
 			List<String> listOfAllergies = new ArrayList<String>();
