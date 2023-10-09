f1e3feef5682b89325b3aeb3d327f80f018f991e
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-09 16:59:38 +0200

    JsonDataLoader.java: add in constructor personService and PersonRepositoryImpl

--- src/main/java/com/safetynet/api/config/JsonDataLoader.java
+++ src/main/java/com/safetynet/api/config/JsonDataLoader.java
@@ -8,55 +8,56 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.safetynet.api.model.MedicalRecord;
+import com.safetynet.api.model.Person;
 import com.safetynet.api.repository.MedicalRecordRepositoryImpl;
+import com.safetynet.api.repository.PersonRepositoryImpl;
 import com.safetynet.api.service.dataservice.MedicalRecordService;
-
-
+import com.safetynet.api.service.dataservice.PersonService;
 
 @Component
-
 public class JsonDataLoader implements CommandLineRunner {
 
-
-
-   // private final PersonService personService;
+    private final PersonService personService;
 
   //  private final FireStationService fireStationService;
 
     private final MedicalRecordService medicalRecordService;
    
-    private final MedicalRecordRepositoryImpl readMedicalRecordsFromfile;
+    private final MedicalRecordRepositoryImpl medicalRecordRepository;
+    private final PersonRepositoryImpl personRepository;
     @Autowired
     public JsonDataLoader(
 
-          //  PersonService personService,
-
-          //  FireStationService fireStationService,
-
+           PersonService personService,
            MedicalRecordService medicalRecordService,
-           MedicalRecordRepositoryImpl readMedicalRecordsFromfile
+           //  FireStationService fireStationService,
+           PersonRepositoryImpl personRepository,    
+           MedicalRecordRepositoryImpl medicalRecordRepository
 
     ) {
 
-       // this.personService = personService;
+        this.personService = personService;
 
        // this.fireStationService = fireStationService;
     	this.medicalRecordService = medicalRecordService;
-        this.readMedicalRecordsFromfile = readMedicalRecordsFromfile;
-
+        this.medicalRecordRepository = medicalRecordRepository;
+        this.personRepository= personRepository;
     }
 
     @Override
-    public void run(String... args) throws IOException {
+    public void run(String... args) {
     	
        try{
 
-    	   List<MedicalRecord> medicalRecords=	readMedicalRecordsFromfile.findAll();
-    	   for(MedicalRecord medicalRecord:medicalRecords) {
-    		   medicalRecordService.addMedicalRecord(medicalRecord);
-    		
+    	   List<MedicalRecord> medicalRecordsFromFile=	medicalRecordRepository.findAll();
+    	   for(MedicalRecord medicalRecord:medicalRecordsFromFile) {
+    		   medicalRecordService.addMedicalRecord(medicalRecord);	
     	   }
 
+    	   List<Person> personsFromFile=	personRepository.findAll();
+    	   for(Person person:personsFromFile) {
+    		   personService.addMedicalRecord(person);	
+    	   }
         } catch (IOException e) {
 
             e.printStackTrace();
