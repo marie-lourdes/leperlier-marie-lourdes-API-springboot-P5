4542c7686a2d2a1b4506ba2cf33758dc4427a7dd
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-09 16:42:07 +0200

    UploadDataFileRunner.java: delete getter and setters

--- src/main/java/com/safetynet/api/UploadDataFileRunner.java
+++ src/main/java/com/safetynet/api/UploadDataFileRunner.java
@@ -4,30 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.safetynet.api.service.dataservice.FireStationService;
+
 import com.safetynet.api.service.dataservice.MedicalRecordService;
-import com.safetynet.api.service.dataservice.PersonService;
+
 @Component
 public class UploadDataFileRunner implements CommandLineRunner {
-	/* private List<FireStation>fireStations ;
-	private  List<MedicalRecord>medicalRecords;
-	private  List<Person>persons;*/
 	 
- @Autowired
- PersonService  personService;
+ /*@Autowired
+ PersonService  personService;*/
  
- @Autowired
- FireStationService  fireStationService;
+/* @Autowired
+ FireStationService  fireStationService;*/
  
  @Autowired
  MedicalRecordService  medicalRecordService;
 
 	@Override
 	public void run(String... args) throws Exception {
+		//medicalRecordService.getAllMedicalRecords();
 		//use to logging each element of file json 
-		personService.getPersonsFromFile();
-	  /* 	fireStationService.getFireStationsFromFile();
-	 medicalRecordService.getMedicalRecordsFromFile();*/
+		//personService.getPersonsFromFile();
+	
 	 
 	   // fireStationService.deleteByStationNumberFireStation("3");
 	  //  System.out.println("firestation APRES SUPPRESION "+	fireStationService.getFireStationsFromFile());
@@ -38,25 +35,5 @@ public class UploadDataFileRunner implements CommandLineRunner {
 		personService.getOnePersonById("Tenley Boyd");
 		medicalRecordService.getOneMedicalRecordById("Tenley Boyd");*/
 	}
-/* public List<FireStation> getFireStation () {
-	 return fireStations;
- }
- public List<FireStation> setFireStation ( List<FireStation> firestations) {
-	 return this.fireStations= firestations ;
- }
- 
- public List<Person> getPerson () {
-	 return persons;
- }
- public List<Person> setPerson ( List<Person> persons) {
-	 return this.persons= persons ;
- }
- 
- public List<MedicalRecord> getMedicalRecord() {
-	 return medicalRecords;
- }
- public  List<MedicalRecord> setMedicalRecord ( List<MedicalRecord> medicalRecords) {
-	 return this.medicalRecords= medicalRecords ;
- }*/
- 
+
 }
--- src/main/java/com/safetynet/api/config/JsonDataLoader.java
+++ src/main/java/com/safetynet/api/config/JsonDataLoader.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import com.safetynet.api.model.MedicalRecord;
 import com.safetynet.api.repository.MedicalRecordRepositoryImpl;
-import com.safetynet.api.service.ReadMedicalRecordDataFromFileImpl;
 import com.safetynet.api.service.dataservice.MedicalRecordService;
 
 
