6871fda3516c5e9725d148b2bc3d255b2c940ab1
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-09 16:19:28 +0200

    refactoring repositoy interface and class implemented of medicalRecord with import code from git repository test JsonNitter

--- src/main/java/com/safetynet/api/repository/MedicalRecordRepositoryImpl.java
+++ src/main/java/com/safetynet/api/repository/MedicalRecordRepositoryImpl.java
@@ -13,10 +13,7 @@ import com.safetynet.api.service.ReadMedicalRecordDataFromFileImpl;
 
 @Component
 public class MedicalRecordRepositoryImpl implements IMedicalRecordRepository {
-	private List<Optional<MedicalRecord>> listOfMedicalRecordsFoundByName;
-	private Optional<MedicalRecord> medicalRecordFoundByName;
-	private Optional<MedicalRecord> medicalRecordFoundById;
-
+	
 	@Autowired
 	ReadMedicalRecordDataFromFileImpl readMedicalRecords;
 
@@ -25,50 +22,5 @@ public class MedicalRecordRepositoryImpl implements IMedicalRecordRepository {
 		return readMedicalRecords.readFile();
 	}
 
-	@Override
-	public List<Optional<MedicalRecord>> findByFirstNameAndLastName(String firstName, String lastName) {
-		List<MedicalRecord> medicalRecords = new ArrayList<MedicalRecord>();
-		listOfMedicalRecordsFoundByName = new ArrayList<Optional<MedicalRecord>>();
-		medicalRecordFoundByName = Optional.empty();
-		
-		try {
-			medicalRecords = readMedicalRecords.readFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		medicalRecords.forEach(elem -> {
-			String firstNameMedicalRecord = elem.getFirstName();
-			String lastNameMedicalRecord = elem.getLastName();
-			if (firstNameMedicalRecord.equals(firstName) && lastNameMedicalRecord.equals(lastName)) {
-				System.out.println("medicalRecord found by first and last name of person" + elem);
 
-				medicalRecordFoundByName = Optional.ofNullable(elem);
-				listOfMedicalRecordsFoundByName.add(medicalRecordFoundByName);
-			}
-		});
-		System.out.println("listOfMedicalRecordsFoundByName:" + listOfMedicalRecordsFoundByName);
-		return listOfMedicalRecordsFoundByName;
-	}
-
-	@Override
-	public Optional<MedicalRecord> findById(String id) {
-		List<MedicalRecord> medicalRecords = new ArrayList<MedicalRecord>();
-		medicalRecordFoundById = Optional.empty();
-		
-		try {
-			medicalRecords = readMedicalRecords.readFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		medicalRecords.forEach(elem -> {
-			String idMedicalRecord = elem.getId();
-			if (idMedicalRecord.toString().equals(id.toString())) {
-						medicalRecordFoundById = Optional.ofNullable(elem);		
-			}
-		});
-		System.out.println("medicalRecordFoundById:" + medicalRecordFoundById);
-		return medicalRecordFoundById;
-	}
 }
