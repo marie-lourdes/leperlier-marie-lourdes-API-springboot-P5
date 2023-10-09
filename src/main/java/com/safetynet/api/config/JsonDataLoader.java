989d9840b183349578450f3f835df28223a5eef4
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-09 16:12:15 +0200

    JsonDataLoader.java: test the commandline runner of class with medicalService and MedicalRecordRepositoryImpl

--- src/main/java/com/safetynet/api/config/JsonDataLoader.java
+++ src/main/java/com/safetynet/api/config/JsonDataLoader.java
@@ -1,5 +1,69 @@
 package com.safetynet.api.config;
 
-public class JsonDataLoader {
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.safetynet.api.model.MedicalRecord;
+import com.safetynet.api.repository.MedicalRecordRepositoryImpl;
+import com.safetynet.api.service.ReadMedicalRecordDataFromFileImpl;
+import com.safetynet.api.service.dataservice.MedicalRecordService;
+
+
+
+@Component
+
+public class JsonDataLoader implements CommandLineRunner {
+
+
+
+   // private final PersonService personService;
+
+  //  private final FireStationService fireStationService;
+
+    private final MedicalRecordService medicalRecordService;
+   
+    private final MedicalRecordRepositoryImpl readMedicalRecordsFromfile;
+    @Autowired
+    public JsonDataLoader(
+
+          //  PersonService personService,
+
+          //  FireStationService fireStationService,
+
+           MedicalRecordService medicalRecordService,
+           MedicalRecordRepositoryImpl readMedicalRecordsFromfile
+
+    ) {
+
+       // this.personService = personService;
+
+       // this.fireStationService = fireStationService;
+    	this.medicalRecordService = medicalRecordService;
+        this.readMedicalRecordsFromfile = readMedicalRecordsFromfile;
+
+    }
+
+    @Override
+    public void run(String... args) throws IOException {
+    	
+       try{
+
+    	   List<MedicalRecord> medicalRecords=	readMedicalRecordsFromfile.findAll();
+    	   for(MedicalRecord medicalRecord:medicalRecords) {
+    		   medicalRecordService.addMedicalRecord(medicalRecord);
+    		
+    	   }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+   }
 
 }
