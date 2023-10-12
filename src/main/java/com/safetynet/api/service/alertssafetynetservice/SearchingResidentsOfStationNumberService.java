17b54b8548a7dfbdddbf3bdd1aeb04ea0ffb690c
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-12 11:09:12 +0200

    SearchingResidentsOfStationNumberService.java:add methods necessary for this service without implementation

--- src/main/java/com/safetynet/api/service/alertssafetynetservice/SearchingResidentsOfStationNumberService.java
+++ src/main/java/com/safetynet/api/service/alertssafetynetservice/SearchingResidentsOfStationNumberService.java
@@ -1,5 +1,29 @@
 package com.safetynet.api.service.alertssafetynetservice;
 
+import org.springframework.stereotype.Service;
+
+import com.safetynet.api.model.FireStation;
+import com.safetynet.api.model.Person;
+
+@Service
 public class SearchingResidentsOfStationNumberService {
+	
+	
+	public Object getResidentsOfStationNumberService(){
+		return null;
+	}
+	
+	public FireStation  getFireStationByStationNumber() {
+		return null;}
+	
+	public Person getPersonByAddress() {
+		return null;
+		}
+	
+	public Person getAgeOfPerson() {
+		 //getBirthDateOfPersonWithMedicalRecord()
+		//calculateAgeOfPerson();
+		return null;
+		}
 
 }
