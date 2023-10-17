98e58aa6cdfcd51fee8442daac4afcf9e996c727
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-17 18:09:50 +0200

    remove code unecessary in FireService and FloodService

--- src/main/java/com/safetynet/api/service/alertssafetynetservice/FloodService.java
+++ src/main/java/com/safetynet/api/service/alertssafetynetservice/FloodService.java
@@ -1,13 +1,9 @@
 package com.safetynet.api.service.alertssafetynetservice;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,40 +30,14 @@ public class FloodService {
 		
 				if(!listOfAddress.contains(itrResidentNextToNext.get("address"))) {	
 					listOfAddress.add(itrResidentNextToNext.get("address"));
-					//itrResidentNextToNext =	itrResidentsOfStationNumber.next();
-				}
-				
-				
+					
+				}		
 		}
+		
 		for(String address:listOfAddress ) {
 			List<Map<String, String>> listOfResidentWithMedicalRecord = new ArrayList<Map<String, String>>();
 			 listOfResidentWithMedicalRecord=searchingFullInfoOfResidentsWithMedicalRecord.searchInfoOfResident(address);
-			/*List<Map<String, String>> listOfHouseHoldSameaddress = searchingFullInfoOfResidentsByAddress.searchInfoOfResident(address);	
-			List<Object> listOfResidentWithMedicalRecord = new ArrayList<Object>();*/
-			/*for (Map<String, String> resident : listOfHouseHoldSameaddress) {
-				
-				Map<String, String> mapOfMedicalRecord = new HashMap<String, String>();
-				Map<String, String> mapOfMedicalRecordOfResidentUpdated = new HashMap<String, String>();
-				String fullNamePerson = resident.get("firstName") + " " + resident.get("lastName");
-				Optional<MedicalRecord> medicalRecordFoundByFullName = medicalRecordService
-						.getOneMedicalRecordById(fullNamePerson);
-				resident.remove("firstName");
-		
-				resident.remove("city");
-				resident.remove("email");
-				mapOfMedicalRecord.put("medications", medicalRecordFoundByFullName.get().getMedications().toString());
-				mapOfMedicalRecord.put("allergies", medicalRecordFoundByFullName.get().getAllergies().toString());
-				
-				mapOfMedicalRecordOfResidentUpdated.put("medicalRecord", mapOfMedicalRecord.toString());
-				mapOfMedicalRecordOfResidentUpdated.put("resident", resident.toString());
-				TreeMap<String, String> treeMapOfMedicalRecordOfResidentUpdated =
-						 new TreeMap<String, String>(Collections.reverseOrder());
-				
-				treeMapOfMedicalRecordOfResidentUpdated.putAll(mapOfMedicalRecordOfResidentUpdated);
-				listOfResidentWithMedicalRecord.add(treeMapOfMedicalRecordOfResidentUpdated);
-				
-			}*/
-			 listOfHouseHoldOfStationNumber.add(listOfResidentWithMedicalRecord);
+					 listOfHouseHoldOfStationNumber.add(listOfResidentWithMedicalRecord);
 			
 		}	
 		System.out.println(" listOfHouseHoldOfStationNumber"+ listOfHouseHoldOfStationNumber);
