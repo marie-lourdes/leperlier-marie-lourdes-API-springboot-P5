3a4725240d56187783ca98322f5e4a79f1ef414b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-18 11:45:36 +0200

    CommunityEmailService.java: inject class SearchingInfoEmailOfResidentsByCitympl and use it in method of service

--- src/main/java/com/safetynet/api/service/alertssafetynetservice/CommunityEmailService.java
+++ src/main/java/com/safetynet/api/service/alertssafetynetservice/CommunityEmailService.java
@@ -1,9 +1,17 @@
 package com.safetynet.api.service.alertssafetynetservice;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommunityEmailService {
 @Autowired
-PersonService personService;
+SearchingInfoEmailOfResidentsByCitympl infoEmailOfResidentsByCity;
+
+public List<Map<String, String>> getEmailOfResidentsOfCity(String city) {
+	return infoEmailOfResidentsByCity.searchInfoOfResident(city);
+}
 }
