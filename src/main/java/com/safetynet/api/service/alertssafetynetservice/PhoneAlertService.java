afd509ff1d5965fccfa7dc02e23716bff8656385
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-23 17:50:12 +0200

    logging-PhoneAlertsService: add log debug,info, error in methods of service

--- src/main/java/com/safetynet/api/service/alertssafetynetservice/PhoneAlertService.java
+++ src/main/java/com/safetynet/api/service/alertssafetynetservice/PhoneAlertService.java
@@ -4,23 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.safetynet.api.service.dataservice.PersonService;
+
 @Service
 public class PhoneAlertService {
+	private static final Logger log = LogManager.getLogger( PhoneAlertService .class);
+	
 	@Autowired
 	SearchingInfoOfResidentOfStationNumberImpl infoOfResidentOfStationNumber;
+	
 	private List<Map<String, String>> listOfPhonesOfResidentOfStationNumber;
 
 	public List<Map<String, String>> getListOfPhonesOfResidentsOfStationNumber(String stationNumber)
 			throws NullPointerException {
+		log.debug("Retrieving all phones of residents of firestation");
 		try {
-
 			listOfPhonesOfResidentOfStationNumber = new ArrayList<Map<String, String>>();
 			listOfPhonesOfResidentOfStationNumber = infoOfResidentOfStationNumber.searchInfoOfResident(stationNumber);
+	
 		} catch (NullPointerException e) {
-			throw new NullPointerException("Not phone of resident found of this firestation");
+			log.error("Failed to retrieve phones of this firestation : {}",stationNumber);
+			throw new NullPointerException("Not phone of resident found of this firestation : "+ stationNumber);
 		}
 
 		for (Map<String, String> residents : listOfPhonesOfResidentOfStationNumber) {
@@ -30,8 +39,7 @@ public class PhoneAlertService {
 			residents.remove("age");
 			System.out.println("residents" + residents);
 		}
-
-		System.out.println("list of phones of resident" + listOfPhonesOfResidentOfStationNumber);
+		log.info("List of phones of residents of firestation retrieved successfully : {}", listOfPhonesOfResidentOfStationNumber);
 		return listOfPhonesOfResidentOfStationNumber;
 	}
 }
