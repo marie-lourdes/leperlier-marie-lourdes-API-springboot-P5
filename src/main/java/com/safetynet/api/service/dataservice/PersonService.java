df66a51bb21b2e56824988eb4f6e13be34b7bf9b
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-28 10:51:19 +0200

    PersonServiceTest: add assertion all in method test testGetOnePersonById()

--- src/main/java/com/safetynet/api/service/dataservice/PersonService.java
+++ src/main/java/com/safetynet/api/service/dataservice/PersonService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.safetynet.api.model.Person;
--- src/test/java/com/safetynet/api/service/dataservice/PersonServiceTest.java
+++ src/test/java/com/safetynet/api/service/dataservice/PersonServiceTest.java
@@ -1,33 +1,58 @@
 package com.safetynet.api.service.dataservice;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.safetynet.api.model.Person;
 
-
 @SpringBootTest
 class PersonServiceTest {
+	private static final Logger log = LogManager.getLogger(PersonServiceTest.class);
 	@Autowired
 	private PersonService personService;
-	@Test
-	void testGetOnePersonById() {
+	private Person person;
 
-      /*  String expected = "John";
-        Person personExpectedById = new Person("John Boyd","John","Boyd","1509 Culver St","Culver","97451","841-874-6512","jaboyd@email.com" );
+	@BeforeEach
+	public void setUpPerTest() {
+		person = new Person("John Boyd", "John", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512",
+				"jaboyd@email.com");
 
-        Person result =  personService.getOnePersonById("John Boyd");
-        
-		assertEquals(personExpectedById,  result);*/
-	   Person resultPerson =  personService.getOnePersonById("John Boyd");
-	   
-		String expectedFirstName = "John ";
-		String expectedLastName = "Boyd";
-        String resultId =resultPerson.getFirstName() +" "+ resultPerson.getLastName();
-			assertEquals(expectedFirstName+expectedLastName,  resultId);
 	}
 
+	@Test
+	void testGetOnePersonById() {
+		try {
+			Person resultPerson = personService.getOnePersonById("John Boyd");
+
+			String expectedFirstName = person.getFirstName();
+			String expectedLastName = person.getLastName();
+			String expectedAddress = person.getAddress();
+			String expectedCity = person.getCity();
+			String expectedZip = person.getZip();
+			String expectedPhone = person.getPhone();
+			String expectedEmail = person.getEmail();
+
+			assertAll("assertion all data of person found by id", () -> assertNotNull(resultPerson),
+					() -> assertEquals(expectedFirstName + expectedLastName, resultPerson.getId(),
+							"the id should  be the first name and last name of person"),
+					() -> assertEquals(expectedFirstName, resultPerson.getFirstName()),
+					() -> assertEquals(expectedLastName, resultPerson.getLastName()),
+					() -> assertEquals(expectedAddress, resultPerson.getAddress()),
+					() -> assertEquals(expectedCity, resultPerson.getCity()),
+					() -> assertEquals(expectedZip, resultPerson.getZip()),
+					() -> assertEquals(expectedPhone, resultPerson.getPhone()),
+					() -> assertEquals(expectedEmail, resultPerson.getEmail()));
+
+		} catch (AssertionError e) {
+			log.error(e.getMessage());
+		}
+	}
 }
