9ad250f4c2f65b6cc8dbf3df1a5706efbcb6aa78
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-10-29 11:45:04 +0100

    AlertController.java: add comments

--- src/main/java/com/safetynet/api/controller/AlertsController.java
+++ src/main/java/com/safetynet/api/controller/AlertsController.java
@@ -111,6 +111,10 @@ public class AlertsController implements IResponseHTTPEmpty {
 		}
 	}
 
+	/* when testing , after resquest all param stations ,
+	 *  if request one no existing keep all data retrived according to stations param, 
+	 *  no fixing this error refactoring the method getListOfHouseHoldByStationNumberIfFlood() with list.clear() or removeall() "
+	 */
 	@GetMapping("/flood/stations")
 	@ResponseBody
 	public ResponseEntity<?> getListOfHouseHoldByStationNumberIfFlood(@RequestParam List<String> stations) {
