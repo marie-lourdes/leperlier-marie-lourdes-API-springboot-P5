68704a18cd654ff417ef70a8f2a2958ae850a2b3
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-03 14:19:06 +0100

    FireStationController.java : remove route and method http get all firestation

--- src/main/java/com/safetynet/api/controller/FireStationController.java
+++ src/main/java/com/safetynet/api/controller/FireStationController.java
@@ -103,16 +103,4 @@ public class FireStationController implements IResponseHTTPEmpty {
 			return new ResponseEntity<Long>(HttpStatus.NOT_FOUND);
 		}
 	}
-	@GetMapping("/firestation/")
-	@ResponseBody
-	public ResponseEntity<?> getAllFireStations() {
-		List<FireStation> allFireStations;
-		try {
-			allFireStations = fireStationService.getAllFireStations();
-			return ResponseEntity.status(HttpStatus.OK).body(allFireStations);
-		} catch (NullPointerException e) {
-			log.error(e.getMessage());
-			return returnResponseEntityEmptyAndCode404();
-		}
-	}
 }
