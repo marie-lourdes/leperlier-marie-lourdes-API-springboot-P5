250fae070253f5208798d08a46bc725f5664d5fe
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-12 14:17:57 +0100

    IResponseHTTPEmpty.java: refactor string empty

--- src/main/java/com/safetynet/api/utils/IResponseHTTPEmpty.java
+++ src/main/java/com/safetynet/api/utils/IResponseHTTPEmpty.java
@@ -6,11 +6,7 @@ import org.springframework.http.ResponseEntity;
 public interface IResponseHTTPEmpty {
 
 	default ResponseEntity<Object> returnResponseEntityEmptyAndCode404() {
-		ResponseEntity<Object> responseEmpty = ResponseEntity.status(HttpStatus.NOT_FOUND).body(new String(" "));
+		ResponseEntity<Object> responseEmpty = ResponseEntity.status(HttpStatus.NOT_FOUND).body(" ");
 		return responseEmpty;
-	}	
-	default ResponseEntity<?> returnResponseEntityEmptyAndCode500() {
-		ResponseEntity<?> responseEmpty = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new String(" "));
-		return responseEmpty;
-	}
+	}		
 }
