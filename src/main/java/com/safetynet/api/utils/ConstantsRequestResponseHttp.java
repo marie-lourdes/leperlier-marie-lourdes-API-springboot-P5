40494f92a9c7bc69610701e7112bc59f2cb968f6
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-24 11:35:38 +0100

    ConstantsRequestResponseHttp.java: add constants request and response for fire station

--- src/main/java/com/safetynet/api/utils/ConstantsRequestResponseHttp.java
+++ src/main/java/com/safetynet/api/utils/ConstantsRequestResponseHttp.java
@@ -14,6 +14,17 @@ public class ConstantsRequestResponseHttp {
 	public static final String RESPONSE_PUT_MEDICALRECORD  = "Response PUT: '/medicalRecord', {}";
 	public static final String REQUEST_DELETE_MEDICALRECORD  = "Request DELETE: ' /medicalRecord', {}";
 	public static final String RESPONSE_DELETE_MEDICALRECORD = "Response DELETE: '/medicalRecord', {}";
+	
+	public static final String REQUEST_POST_STATIONNUMBER_OF_FIRESTATION = "Request POST: '/firestation', {}";
+	public static final String RESPONSE_POST_STATIONNUMBER_OF_FIRESTATION = "Response POST: '/firestation', {}";
+	public static final String REQUEST_POST_ADDRESS_OF_FIRESTATION = "Request POST: '/firestation/{stationNumber}', {}";
+	public static final String RESPONSE_POST_ADDRESS_OF_FIRESTATION = "Response POST: '/firestation/{stationNumber}', {}";
+	public static final String REQUEST_PUT_FIRESTATION   = "Request PUT: '/firestation', {}";
+	public static final String RESPONSE_PUT_FIRESTATION   = "Response PUT: '/firestation', {}";
+	public static final String REQUEST_DELETE_STATIONNUMBER_OF_FIRESTATION = "Request DELETE: '/firestation', {}";
+	public static final String REQUEST_DELETE_ADDRESS_OF_FIRESTATION = "Request DELETE: '/firestation/{stationNumber}', {}";
+	public static final String RESPONSE_DELETE_STATIONNUMBER_OF_FIRESTATION = "Response DELETE: '/firestation', {}";
+	public static final String RESPONSE_DELETE_ADDRESS_OF_FIRESTATION = "Response DELETE: '/firestation/{stationNumber}', {}";
 
 	private  ConstantsRequestResponseHttp () {
 
