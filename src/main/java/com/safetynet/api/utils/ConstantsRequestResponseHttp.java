4a1abf10d4b88b9bb7fddb3859c1815ba531a46c
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-24 10:57:29 +0100

    create class util ConstantsRequestResponseHttp.java

---
+++ src/main/java/com/safetynet/api/utils/ConstantsRequestResponseHttp.java
@@ -0,0 +1,14 @@
+package com.safetynet.api.utils;
+
+public class ConstantsRequestResponseHttp {
+	public static final String REQUEST_POST_PERSON = "Request POST: /person {}";
+	public static final String RESPONSE_POST_PERSON = "Response POST: /person {}";
+	public static final String REQUEST_PUT_PERSON = "Request PUT: /person {}";
+	public static final String RESPONSE_PUT_PERSON = "Response PUT: /person {}";
+	public static final String REQUEST_DELETE_PERSON = "Request DELETE: /person {}";
+	public static final String RESPONSE_DELETE_PERSON = "Response DELETE: /person {}";
+
+	private  ConstantsRequestResponseHttp () {
+
+	}
+}
