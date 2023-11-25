8972dd5be8f597c7a446586ba0a2de6617746bc2
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-11-25 19:18:39 +0100

    HotFix logging info configuration properties  on release v1.0.6

--- src/main/java/com/safetynet/api/controller/AlertsController.java
+++ src/main/java/com/safetynet/api/controller/AlertsController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
